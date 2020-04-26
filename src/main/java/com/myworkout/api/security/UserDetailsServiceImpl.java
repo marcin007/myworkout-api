b3efee436a6e384aeedd6b6e0586483eb8c3be93
marcin007 <marcin007wojciechowski@gmail.com>
2020-04-26 22:22:38 +0200

    Introduce to security.

---
+++ src/main/java/com/myworkout/api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.myworkout.api.security;
+
+import com.myworkout.api.entity.User;
+import com.myworkout.api.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found by username: " + username));
+
+        System.out.println(user.getUsername());
+        logger.debug("Próba autoryzacji.. " + user.getUsername());
+
+        return new UserDetailsImpl(user);
+    }
+}
